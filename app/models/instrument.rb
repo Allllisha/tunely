0694026b701cfdaeb5719c05a4dae3c9025d3b9d
Tom Tsui <tomtsui2642@gmail.com>
2022-01-22 18:11:46 +0900

    add validation, association, enum to Instrument model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,16 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+
+  validates :category, :location, :renting_price, presence: true
+  validates :renting_price, inclusion: { in: 1.. }, numericality: { only_integer: true }
+
+  enum category: [
+    :trumpet,
+    :violin
+  ]
+
+  enum condition: [
+    :good,
+    :bad
+  ]
 end
