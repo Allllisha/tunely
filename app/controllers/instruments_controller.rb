21bbfccd399c50382911eb3a0516dcd5b5363175
RefMags <fyfymags@gmail.com>
2022-01-25 22:10:34 +0900

    pundit and controllers skeleton added: bookings,instruments & owners

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,28 +1,13 @@
 class InstrumentsController < ApplicationController
   def index
-    # @instruments = Instruments.all
   end
 
   def show
-    # @instrument = Instrument.find(params[:id])
   end
 
   def new
-    # @instrument = Instrument.new
   end
 
   def create
-    # @instrument = Instrument.new(instrument_params)
-    # if @instrument.save
-    #   redirect_to instrument_path(@instrument)
-    # else 
-    #   render :new
-  end
-
-  private 
-
-  def instrument_params
-    #do we permit everything from table?
-    # params.require(:instrument).permit(:category, :brand, :location, :condition, :availability)
   end
 end
