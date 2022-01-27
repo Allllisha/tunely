471b15c59356332dce4e66d2c7681803596fc148
Tom Tsui <tomtsui2642@gmail.com>
2022-01-27 20:11:28 +0900

    fixed routes.rb spelling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :intruments, only: %i[index new create show] do
+  resources :instruments, only: %i[index new create show] do
     resources :bookings, only: %i[create]
   end
   resources :bookings, only: %i[index update]
