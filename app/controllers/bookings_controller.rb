b16aec1ea84c139c441bd988f331519fc96ca9bd
RefMags <fyfymags@gmail.com>
2022-01-27 21:07:58 +0900

    routes added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,26 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.instrument = @instrument
+   if @booking.save
+    redirect_to booking_path(@booking)
+   else
+    render :new
+   end
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
