add4de1a39333e860fb5f15bdfed938079b0ae32
Tom Tsui <tomtsui2642@gmail.com>
2022-01-27 21:50:13 +0900

    instrument#create done

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -11,5 +11,18 @@ class InstrumentsController < ApplicationController
   end
 
   def create
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
+    authorize @instrument
+    @instrument.save!
+    redirect_to owner_bookings_path
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(
+      :category, :brand, :location, :condition, :renting_price
+    )
   end
 end
