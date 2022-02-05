58e703e1ace8366bcfcabd9ae347c93a6ebf6931
Tom Tsui <tomtsui2642@gmail.com>
2022-02-05 15:53:30 +0900

    passed dates to calendar js

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,12 +2,12 @@ import { Controller } from "stimulus";
 import * as LibName from "vanilla-js-calendar";
 
 export default class extends Controller {
-  static targets = [ 'calender' ];
+  static targets = [ 'calendar' ];
 
   connect() {
     const JSCalendar = LibName.JSCalendar;
     const JSCalendarEvent = LibName.JSCalendarEvent;
-    const calendar = new JSCalendar(this.calenderTarget, {
+    const calendar = new JSCalendar(this.calendarTarget, {
       views: ["month"],
       daysVocab: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
       width: 300,
@@ -17,5 +17,8 @@ export default class extends Controller {
       .render();
     document.querySelector(".control-bar-views").remove();
     document.querySelector(".calendar-action-today").remove();
+    this.calendarTarget.dataset.bookings.split(',').forEach(startAndEnd => {
+      startAndEnd.split('|');
+    });
   }
 }
