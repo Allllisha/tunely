ca1e010c461bccb3df0209f9ecded23621b20032
Nico L <l.nico08@laposte.net>
2022-02-05 11:29:44 +0900

    owner bookings controller first elements

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,5 +1,14 @@
 class Owner::BookingsController < ApplicationController
   def index
-    @bookings = policy_scope(Booking).where(user_id: current_user.id)
+    @instruments = policy_scope(Instrument).where(user_id: current_user.id)
+    @instruments_with_bookings = []
+    @instruments_without_bookings = []
+    @instruments.each do |instrument|
+      if instrument.bookings.present?
+        @instruments_with_bookings << instrument
+      else
+        @instruments_without_bookings << instrument
+      end
+    end
   end
 end
