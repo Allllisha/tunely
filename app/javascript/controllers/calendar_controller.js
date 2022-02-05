1a9d3b5dd5ac5c06dade9b30d31f58de60d7ea07
Tom Tsui <tomtsui2642@gmail.com>
2022-02-05 12:39:44 +0900

    imported vanlilla-js-calendar into instruments/show.html

---
+++ app/javascript/controllers/calendar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus";
+import * as LibName from "vanilla-js-calendar";
+
+export default class extends Controller {
+  static targets = [ 'calender' ];
+
+  connect() {
+    const JSCalendar = LibName.JSCalendar;
+    const JSCalendarEvent = LibName.JSCalendarEvent;
+    const calendar = new JSCalendar(this.calenderTarget, {
+      /* options */
+    })
+      .init()
+      .render();
+
+  }
+}
